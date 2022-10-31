ef060f23e90056e6c5aef416fe84b6f8cfc55153
Eddie Rodriguez <edwardjrodriguez17@gmail.com>
2022-10-31 15:55:24 -0600

    Initial commit

---
+++ src/components/App/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+// import React, {useState} from 'react';
+import { Routes, Route, useNavigate } from "react-router-dom"
+
+
+function App() {
+  const navigate = useNavigate()
+
+
+  return (
+    <div className="App">
+    <Routes>
+    
+      <Route path='*' element={
+        <div>
+          <h1 className='not-found'>404: Not found</h1>
+          <button className='search-page' onClick={() => navigate('/home')} >Back to Search Page</button>
+        </div>
+      } />
+    </Routes>
+  </div>
+  );
+}
+
+export default App;
