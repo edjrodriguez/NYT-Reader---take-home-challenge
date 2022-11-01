b09cb9b2a78e480c913870073eec4df7000d1a5d
Eddie Rodriguez <edwardjrodriguez17@gmail.com>
2022-11-01 14:38:10 -0600

    Add fetch call with env file

--- src/components/ArticleCard/ArticleCard.js
+++ src/components/ArticleCard/ArticleCard.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ArticleCard = () => {
 
-    return(
+    return (
         <>
             <h1>article deets lorem ipsum huzzah! </h1>
             <p>An image will be here</p>
