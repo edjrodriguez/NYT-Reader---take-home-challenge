92ebce2c91168c9e711c27577f39225aed5f1af1
Eddie Rodriguez <edwardjrodriguez17@gmail.com>
2022-11-02 11:41:25 -0600

    Render article details page

--- src/components/ArticlePage/ArticlePage.js
+++ src/components/ArticlePage/ArticlePage.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { useNavigate } from "react-router-dom"
-import ArticleCard from '../ArticleCard/ArticleCard';
 
 const ArticlePage = ({ moreDetails }) => {
+    const image = moreDetails.multimedia[0].url
     const navigate = useNavigate()
-
     return (
         <>
-            <ArticleCard
-                moreDetails={moreDetails}
-            />
-            <button onClick={() => navigate('/home')}> Back to Headlines</button>
+            <button onClick={() => navigate('/')}> Back to Headlines</button>
+            <h1>{moreDetails.title}</h1>
+            <h3>{moreDetails.byline}</h3>
+            <p>{moreDetails.published_date}</p>
+            <img src={`${image}`} alt="from this NYT article"/>
+            <h4>{moreDetails.abstract}</h4>
+            <a href={moreDetails.short_url}>Read full article</a>
         </>
     )
 }
